7438c2642f7e102e9980fa3b8426c7feb6b9cc3a
Hirotake Tomiyama <e155748@ie.u-ryukyu.ac.jp>
2017-12-14 15:17:26 +0900

    test

---
+++ teamC/CharacterHP.swift
@@ -0,0 +1,16 @@
+//
+//  HP.swift
+//  teamC
+//
+//  Created by Hirotake Tomiyama on 2017/12/14.
+//  Copyright © 2017年 河野佑亮. All rights reserved.
+//
+
+import UIKit
+
+let stand_sleep_time = 7.5 //目安となる睡眠時間(7時間30分)
+var sleep_time = 0.0       //睡眠時間
+var hit_point = 100        //HP
+
+
+
--- teamC/HP.swift
+++
@@ -1,9 +0,0 @@
-//
-//  HP.swift
-//  teamC
-//
-//  Created by Hirotake Tomiyama on 2017/12/14.
-//  Copyright © 2017年 河野佑亮. All rights reserved.
-//
-
-import Foundation
