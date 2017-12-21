84d5a670f645e594050c6ad2ff47f7905c31cf17
Hirotake Tomiyama <e155748@ie.u-ryukyu.ac.jp>
2017-12-21 11:54:54 +0900

    HPと経験値

--- teamC/CharacterHP.swift
+++ teamC/CharacterHP.swift
@@ -12,6 +12,7 @@ let stand_sleep_time: Float = 7.5 //目安となる睡眠時間(7時間30分)
 var sleep_time: Float = 1         //睡眠時間
 var hit_point: Float = 100        //HP
 var per_time: Float = 0
+var exp_point: Float = 0          //経験値
 let n: Float = 1                  //HPの減り方を見て調節
 
 
@@ -19,12 +20,13 @@ let n: Float = 1                  //HPの減り方を見て調節
 class CharacterHP {
     func HP(sleep: Float){
         per_time = sleep_time / stand_sleep_time
-        if per_time >= 1 {
-            per_time = per_time - 1
-            hit_point = hit_point - (per_time * n)
-        } else if per_time < 1 {
+        if per_time < 1 {
+            //睡眠時間が少ないとHPを減らす
             per_time = 1 - per_time
             hit_point = hit_point - (per_time * n)
+        } else if per_time >= 1 {
+            //睡眠時間が多ければ経験値を増やす
+            exp_point = exp_point + per_time
         }
     }
 }
