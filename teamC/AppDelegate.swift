1acca324682a85ddfa78703d3f7dc1e34d4f9b8d
Hirotake Tomiyama <e155748@ie.u-ryukyu.ac.jp>
2018-01-25 11:22:52 +0900

    new

--- teamC/AppDelegate.swift
+++ teamC/AppDelegate.swift
@@ -25,7 +25,51 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
 
     func applicationDidEnterBackground(_ application: UIApplication) {
-
+        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        
+        print("aaa")
+        let suimin = Filewrite()
+        application.cancelAllLocalNotifications()
+        
+        suimin.a()
+        
+        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        application.cancelAllLocalNotifications()
+        
+        let notification = UILocalNotification()
+        notification.alertAction = "アプリに戻る"
+        notification.alertBody = "ランキングが更新されました"
+        notification.fireDate = NSDate(timeIntervalSinceNow:10) as Date
+        notification.soundName = UILocalNotificationDefaultSoundName
+        // アイコンバッジに1を表示
+        notification.applicationIconBadgeNumber = 1
+        // あとのためにIdを割り振っておく
+        notification.userInfo = ["notifyId": "ranking_update"]
+        application.scheduleLocalNotification(notification)
+        var i = 0
+        var count = 1
+        counter = 0
+        while i == 0 {
+            if count%100000000 == 0{
+                let date = NSDate()
+                let calendar = NSCalendar(calendarIdentifier: NSCalendar.Identifier.gregorian)!
+                let calendarComponents = calendar.components(.hour, from: date as Date)
+                let calendarComponents2 = calendar.components(.minute, from: date as Date)
+                let calendarComponents3 = calendar.components(.second, from: date as Date)
+                let hour = calendarComponents.hour
+                let minute = calendarComponents2.minute
+                let second = calendarComponents3.second
+                var String_a = String(format: "%2d %2d %02d \n",hour!,minute!,second!)
+                print(String_a)
+                if hour == 23 && minute == 53 && second! <= 2{
+                    suimin.sleep_time()
+                    break
+                }
+            }
+            count += 1
+        }
         
     }
 
