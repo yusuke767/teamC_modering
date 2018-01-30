62c7234cb122eb38abf531c50157b62a4ad2e443
Yuri Soku <e165708@ie.u-ryukyu.ac.jp>
2018-01-30 23:09:51 +0900

    タイトル画面つくりました

--- teamC/ViewController.swift
+++ teamC/ViewController.swift
@@ -18,29 +18,40 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         // 背景
         self.view.backgroundColor = UIColor.white
+        setBackgroundImage()
         //BGM
         setSystemSound("hyposomniac_nightmere")
-        //ラベル
-        let label = UILabel()
-        label.text = "アプリ名"
-        label.font = UIFont(name: "HiraMinProN-W3", size: 80)
-        label.sizeToFit()
-        label.center = CGPoint(x: 200, y: 100) // UILabelの中央座標を (200, 100) にする
-        label.layer.position = CGPoint(x: self.view.frame.width/2, y: self.view.frame.width/3)
-        self.view.addSubview(label)
-        
-        //ボタン
-        let nextbutton: UIButton = UIButton(frame: CGRect(x: 0,y: 0, width: 120, height: 50))
-       // ボタンの位置を指定する.
-        nextbutton.layer.position = CGPoint(x: self.view.frame.width/2, y: self.view.bounds.height-200)
-        nextbutton.backgroundColor = UIColor.gray //色
-        nextbutton.layer.masksToBounds = true
-        nextbutton.setTitle("はじめる", for: .normal)// ボタンを押していないときの設定
-        nextbutton.setTitleColor(UIColor.white, for: .normal)
-        nextbutton.layer.cornerRadius = 20.0
-        nextbutton.addTarget(self, action: #selector(ViewController.goNext(_:)), for: .touchUpInside)
+        //ロゴ
+        setLogo()
+        let screenbutton: UIButton = UIButton(frame: CGRect(x: 0,y: 0, width: self.view.frame.width, height: self.view.frame.height))
+        // ボタンの位置を指定する.
+        screenbutton.center = CGPoint(x:self.view.frame.width/2, y:self.view.frame.height/2)
+        screenbutton.backgroundColor = UIColor.clear //色
+        screenbutton.layer.masksToBounds = true// ボタンを押していないときの設定
+        screenbutton.addTarget(self, action: #selector(ViewController.goNext(_:)), for: .touchUpInside)
         //nextbutton.sizeToFit()
-        self.view.addSubview(nextbutton)
+        self.view.addSubview(screenbutton)
+        
+        //文字表示
+        // ボタンのサイズを定義.
+        let bWidth: CGFloat = self.view.bounds.width
+        let bHeight: CGFloat = 100
+        // 配置する座標を定義
+        let posX: CGFloat = self.view.bounds.width/2 - bWidth/2
+        let posY: CGFloat = self.view.bounds.height*4/5 - bHeight/2
+        let nexttext: UILabel = UILabel(frame: CGRect(x: posX, y: posY, width: bWidth, height: bHeight))
+        //テキストを白に
+        nexttext.textColor = UIColor.white
+        // UILabelに文字を代入.
+        nexttext.text = "Touch The Screen"
+        //フォント
+        nexttext.font = UIFont(name: "Copperplate-Light", size: 40)
+        // Textを中央寄せにする.
+        nexttext.textAlignment = NSTextAlignment.center
+        // ViewにLabelを追加.
+        self.view.addSubview(nexttext)
+
+        
     }
     
     func setSystemSound(_ BGM:String?) {
@@ -78,6 +89,49 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    //背景画像設定
+    func setBackgroundImage() {
+        var imageView:UIImageView!
+        // インスタンスの生成
+        let image = UIImage(named:"titleback.png")!
+        imageView = UIImageView(image:image)
+        //画面幅取得
+        let screenWidth:CGFloat = view.frame.size.width
+        let screenHeight:CGFloat = view.frame.size.height
+        // 画像の幅・高さの取得
+        let width:CGFloat = image.size.width
+        let height:CGFloat = image.size.height
+        // 画像サイズをスクリーン高さに合わせる
+        let scale:CGFloat = screenHeight / height
+        let rect:CGRect = CGRect(x:0, y:0, width:width*scale, height:height*scale)
+        // ImageView frame をCGRectで作った矩形に合わせる
+        imageView.frame = rect;
+        // 画像の中心を画面の中心に設定
+        imageView.center = CGPoint(x:screenWidth/2, y:screenHeight/2)
+        //画像をUIImageViewの左上に表示
+        self.view.contentMode = UIViewContentMode.topLeft
+        // 画像を追加し、最背面に設定
+        self.view.addSubview(imageView)
+        self.view.sendSubview(toBack:  imageView)
+    }
+    //タイトルロゴ表示設定
+    func setLogo() {
+        var imageView:UIImageView!
+        let image = UIImage(named:"titlelogo.png")!
+        imageView = UIImageView(image:image)
+        let screenWidth:CGFloat = view.frame.size.width
+        let screenHeight:CGFloat = view.frame.size.height
+        let width:CGFloat = image.size.width
+        let height:CGFloat = image.size.height
+        let scale:CGFloat = screenHeight / height
+        let rect:CGRect = CGRect(x:0, y:0, width:width*scale, height:height*scale)
+        imageView.frame = rect;
+        imageView.center = CGPoint(x:screenWidth/2, y:screenHeight/2)
+        self.view.contentMode = UIViewContentMode.topLeft
+        self.view.addSubview(imageView)
+    }
+    
 
 
 }
